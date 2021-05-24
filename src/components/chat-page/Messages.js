f6ef298c079e41dd2797ccea97f3fda75dede5ae
amarpsp10 <amarpsp10@gmail.com>
2021-05-24 05:43:37 -0700

    chat feature - message sending and receieving implemented and showing on UI

--- src/components/chat-page/Messages.js
+++ src/components/chat-page/Messages.js
@@ -1,4 +1,4 @@
-import React,{useEffect, useState} from 'react'
+import React,{useEffect, useState,useRef} from 'react'
 import './Messages.css'
 import UserCard from './UserCard'
 import {useAuth} from './../../contexts/AuthContext'
@@ -7,11 +7,13 @@ import SendIcon from '@material-ui/icons/Send';
 import {Button} from '@material-ui/core'
 import {Form} from 'react-bootstrap'
 import {userdb,db} from './../../firebase'
-
+import firebase from 'firebase'
 export default function Messages() {
     const{currentUser} = useAuth();
     const[chatUsers,setChatUsers] = useState(null);
     const[chatMessages, setChatMessages] = useState(null);
+    const messageInputRef = useRef();
+    const[chatId, setChatId] = useState(null);
     const CreateMessage = (props) =>{
         if(props.data.sender===currentUser.uid)
         return(
@@ -44,7 +46,10 @@ export default function Messages() {
         
         try{
             await db.collection('chats').where('users','array-contains-any',[id,currentUser.uid]).onSnapshot(snap=>{
-                snap.docs.map(data=>{ChatMessages(data.id)})
+                snap.docs.map(data=>{
+                    ChatMessages(data.id);
+                    setChatId(data.id)
+                })
             })
         } catch{
             alert('something went wrong')
@@ -52,13 +57,30 @@ export default function Messages() {
     }
     async function ChatMessages(chatId){
         try{
-            await db.collection(`chats/${chatId}/messages`).onSnapshot(snap=>{
+            await db.collection(`chats/${chatId}/messages`).orderBy('timestamp', 'asc').onSnapshot(snap=>{
                setChatMessages(snap.docs.map(data=>data.data()));
             })
         } catch{
             console.log('something went wrong')
         }
       }
+
+    async function SendMessage(e){
+      e.preventDefault();
+      if(messageInputRef.current.value !== ''){
+          if(chatId!==null){
+              try{
+                  db.collection(`chats/${chatId}/messages`).add({
+                      message:messageInputRef.current.value,
+                      sender:currentUser.uid,
+                      timestamp:firebase.firestore.FieldValue.serverTimestamp(),
+                    })
+                }catch{
+                    console.log('something went wrong!!')
+                }
+            }
+        }
+    }
     
     return (
         <div className='message-page'>
@@ -104,10 +126,9 @@ export default function Messages() {
                          <img src={Default}/>
                          <text>Amar Preet Singh</text>
                      </div>
-                     <Form>
-
+                     <Form onSubmit={(e)=>SendMessage(e)}>
                      <div className='message-section-chat-input'>
-                         <input placeholder=' message:)'/>
+                         <input placeholder=' message:)' ref={messageInputRef}/>
                          <Button
                              type='submit'
                               variant="contained"
@@ -117,6 +138,7 @@ export default function Messages() {
                             Send
                            </Button>
                      </div>
+                     </Form>
                      {chatMessages==null?
                          <div><text>hey click on user and chat</text></div>
                          :
@@ -126,14 +148,7 @@ export default function Messages() {
                              })}
                          </div> 
                     }
-                     {/* <div className={'chat-box'}>
-                         
-                            <Message sender={true} text={'hi bahi'}/>
-                            <Message sender={false} text={'hello brother'}/>
-                            <Message sender={false} text={'or bta kaisa h'}/>
-                     </div> */}
-                
-                    </Form>
+                    
                   </div>
               </div>
         </div>
