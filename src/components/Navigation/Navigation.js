edcc055e1a4666f6e9a327e1fdc32ac7ab34a740
amarpsp10 <amarpsp10@gmail.com>
2021-06-01 22:04:06 -0700

    Feedback dialog added

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -10,6 +10,17 @@ import DrawerMenu from './Drawer'
 import Default from './../../assets/default.jpg'
 import CreateTopic from './CreatePost'
 import {userdb, db} from './../../firebase'
+import Fab from '@material-ui/core/Fab';
+import FeedbackIcon from '@material-ui/icons/Feedback';
+import Tooltip from '@material-ui/core/Tooltip';
+// import Feedback from './Feedback'
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Rating from '@material-ui/lab/Rating';
+import Box from '@material-ui/core/Box';
 const Navigation = () =>{
     const {signup,login,currentUser} = useAuth()   
     const history = useHistory();
@@ -23,8 +34,11 @@ const Navigation = () =>{
     const [loading, setLoading] = useState(false)
     const[loginModal,setLoginModal] = useState(false);
     const[signUpModal, setSignupModal] = useState(false);
-  
     const[user, setUser] = useState(null);
+    const[feedback, setfeedback] = useState(false);
+    const [value, setValue] = React.useState(2);
+    const [hover, setHover] = React.useState(-1);
+
      useEffect(()=>{
            GetUser();
     },[])
@@ -116,6 +130,10 @@ const Navigation = () =>{
         setSignupModal(false);
         setLoading(false);
     }
+    
+    const feedbackClose = () => {
+      setfeedback(false);
+    };
 
      return(
          <div>
@@ -227,8 +245,64 @@ const Navigation = () =>{
                               </div>
                             </Form>
             </Modal>
+            <Tooltip title="Feedback">
+            <div style={{position:'fixed',right:'3%',bottom:'5%'}}>
+            <Fab onClick={()=>{setfeedback(true)}} color="primary" aria-label="add">
+               <FeedbackIcon />
+            </Fab>
+            </div>
+            </Tooltip>
+            <div>
+              <Dialog
+                open={feedback}
+                keepMounted
+                onClose={feedbackClose}
+                aria-labelledby="alert-dialog-slide-title"
+                aria-describedby="alert-dialog-slide-description"
+              >
+                <DialogTitle id="alert-dialog-slide-title">{"FeedBack"}</DialogTitle>
+                <DialogContent>
+                  <div style={{display:'flex',flexDirection:'column',width:'400px'}}>
+                  <TextField  label="What do like most?" />
+                  <TextField className='my-3' label="not liked ?" />
+                  </div>
+                  <div style={{display:'flex'}}>
+                  <Rating
+                      name="hover-feedback"
+                      value={value}
+                      precision={0.5}
+                      onChange={(event, newValue) => {
+                        setValue(newValue);
+                      }}
+                      onChangeActive={(event, newHover) => {
+                        setHover(newHover);
+                      }}
+                      size='large'
+                      />
+                    {value !== null && <Box style={{alignSelf:'center'}} ml={2}>{labels[hover !== -1 ? hover : value]}</Box>}              
+                  </div>
+                  <TextField className='my-3' label="Description" multiline fullWidth/>
+                </DialogContent>
+                  <Button style={{marginBlock:'8px',marginInline:'20px'}} onClick={feedbackClose} variant='contained' color="primary">
+                    Submit
+                  </Button>
+              </Dialog>
+            </div>
          </div>
      );
 }
+
+const labels = {
+  0.5: 'Useless',
+  1: 'Useless+',
+  1.5: 'Poor',
+  2: 'Poor+',
+  2.5: 'Ok',
+  3: 'Ok+',
+  3.5: 'Good',
+  4: 'Good+',
+  4.5: 'Excellent',
+  5: 'Excellent+',
+};
 
 export default Navigation;
