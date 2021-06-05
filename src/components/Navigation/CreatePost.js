73129ce848957086fc8fe34cfbe93a236f1dcb78
amarpsp10 <amarpsp10@gmail.com>
2021-06-05 06:00:08 -0700

    Create Post new Dialog window created

--- src/components/Navigation/CreatePost.js
+++ src/components/Navigation/CreatePost.js
@@ -11,6 +11,27 @@ import MuiAlert from '@material-ui/lab/Alert';
 import firebase from 'firebase'
 import {UpdateCoins} from './../../apis/API'
 import JoditEditor from "jodit-react";
+import { makeStyles } from '@material-ui/core/styles';
+import Dialog from '@material-ui/core/Dialog';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItem from '@material-ui/core/ListItem';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import CloseIcon from '@material-ui/icons/Close';
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    position: 'relative',
+  },
+  title: {
+    marginLeft: theme.spacing(2),
+    flex: 1,
+  },
+}));
 
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
@@ -20,13 +41,14 @@ function Alert(props) {
 }
 export default function CreatePost() {
   const{currentUser} = useAuth();
+  const classes = useStyles();
   const [open, setOpen] = React.useState(false);
   const [blankError, setBlankError] = useState(false);
   const[loading,setLoading] = useState(false);
   const titleRef = useRef();
   const tagRef = useRef();
   const descriptionRef = useRef();
-
+  
   const handleClickOpen = () => {
     setOpen(true);
   }
@@ -35,6 +57,7 @@ export default function CreatePost() {
   };
    async function handlePost(e){
      e.preventDefault();
+     console.log('sldfj')
      if(titleRef.current.value=='' || tagRef.current.value=='' || descriptionRef.current.value==''){
        return setBlankError(true);
      }
@@ -63,9 +86,9 @@ export default function CreatePost() {
   return (
     <div>
       <div className={'post-icon-box'}>
-      <AddIcon style={{fontSize:'40px',color:'#E3E3E3'}} onClick={handleClickOpen}/>
+      <AddIcon style={{fontSize:'40px',color:'#E3E3E3'}} onClick={(e)=>handleClickOpen(e)}/>
       </div>
-      <Modal isOpen={open} onRequestClose={()=>handleClose} 
+      {/* <Modal isOpen={open} onRequestClose={()=>handleClose} 
                            style={{
                             content : {
                                 borderRadius: '20px',
@@ -107,12 +130,55 @@ export default function CreatePost() {
                                     <Button className={'mx-3'} disabled={loading}  variant='contained' color='primary' type='submit'>Post</Button>
                               </Form>            
                            </div>
-            </Modal>
+            </Modal> */}
+               <Dialog fullScreen open={open} TransitionComponent={Transition}>
+                  <AppBar className={classes.appBar} style={{backgroundColor:'#444753'}}>
+                    <Toolbar>
+                      <IconButton edge="start" color="inherit" onClick={handleClose} aria-label="close">
+                        <CloseIcon />
+                      </IconButton>
+                      <Typography variant="h6" className={classes.title}>
+                        Create Post
+                      </Typography>
+                      {/* <Button variant='outlined' color='primary' disabled={loading} onClick={handlePost}>
+                        Post
+                      </Button>  */}
+                    </Toolbar>
+                  </AppBar>
+                  <div className={'create-post-modal'}>
+                            <Form onSubmit={handlePost}> 
+                              <Form.Group controlId="exampleForm.ControlInput1">
+                                 <Form.Label style={{fontWeight:'bold'}}>Title</Form.Label>
+                                 <Form.Control type="title" placeholder="Title" ref={titleRef} />
+                               </Form.Group>
+                               <Form.Group controlId="exampleForm.ControlSelect1">
+                                  <Form.Label style={{fontWeight:'bold'}}>Category</Form.Label>
+                                  <Form.Control as="select" ref={tagRef}>
+                                     <option>General</option>
+                                     <option>Internship</option>
+                                     <option>Question</option>
+                                      <option>Experience</option>
+                                      </Form.Control>
+                                    </Form.Group>
+                                    <Form.Group controlId="exampleForm.ControlTextarea1">
+                                      <Form.Label style={{fontWeight:'bold'}}>Description</Form.Label>
+                                      <div >
+                                      <JoditEditor
+                                        ref={descriptionRef}
+                                        tabIndex={1} 
+                                        />
+                                        </div>
+                                    </Form.Group>
+                                    <Button variant='outlined' color='primary' onClick={()=>handleClose()}> cancel</Button>
+                                    <Button className={'mx-3'} disabled={loading}  variant='contained' color='primary' type='submit'>Post</Button>
+                              </Form>            
+                           </div>
             <Snackbar open={blankError} autoHideDuration={2000} onClose={handleCloseError}>
                  <Alert onClose={handleCloseError} severity="info">
                   Post Cann't be blank!!
               </Alert>
             </Snackbar>
+                </Dialog>
     </div>
   );
 }
