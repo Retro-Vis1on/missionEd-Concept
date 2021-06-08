419cbbda4f616d42a122c2d0480971c5bc290b5d
amarpsp10 <amarpsp10@gmail.com>
2021-06-08 07:52:14 -0700

    Notification API added, it creates notification when user create post, comment and message someone first time for getting coins. also create notification for follwers

--- src/apis/NotificationApi.js
+++ src/apis/NotificationApi.js
@@ -1,13 +1,23 @@
 import firebase from 'firebase'
 import {userdb, db} from '../firebase'
 
+//condition for notification 
+// 1. getting coins by commenting , creating post, messsaging someone
+// 2. when someone started following you
 
-export async function UpdateCoins(uid, coins){
+// const notifications=[
+//     {msg:'hey you get 10 Coins for creating post !!'},
+//     {msg:'hey you get 2 coins for commenting!!'},
+//     {msg:'hey you get 5 coins for messaging'},
+//     {msg:'Started following you!'},
+// ]
+export async function UpdateNotificationForCoins(uid, coins, reason){
     try{
-       await userdb.doc(uid).get().then((doc)=>{
-           userdb.doc(uid).update({
-               coins: coins+doc.data().coins,
-           })
+       await db.collection(`users/${uid}/notifications`).add({
+           msg: `hey you get ${coins} coins for ${reason}`,
+           seen:false,
+           timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+           coins:true,
        })
     }
     catch{
@@ -16,6 +26,21 @@ export async function UpdateCoins(uid, coins){
     }
 }
 
+export async function UpdateNotificationForFollowers(uid, username, uid2){
+    try{
+       await db.collection(`users/${uid2}/notifications`).add({
+           msg: `${username} started following you`,
+           seen:false,
+           timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+           coins:false,
+           follower: uid,
+       })
+    }
+    catch{
+        console.log('error in updating coins');
+        return 'error in updating coins'
+    }
+}
 
 
 
