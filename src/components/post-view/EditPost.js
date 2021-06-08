bf007f59a2dc09e0432547b41a345fe397e0b3cd
amarpsp10 <amarpsp10@gmail.com>
2021-06-07 22:45:57 -0700

    conflict resolved

--- src/components/post-view/EditPost.js
+++ src/components/post-view/EditPost.js
@@ -30,7 +30,6 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-import JoditEditor from "jodit-react";
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
