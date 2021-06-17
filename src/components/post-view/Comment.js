7e48440afdd2e8e8a8603322104c9598883bdb38
amarpsp10 <amarpsp10@gmail.com>
2021-06-17 06:29:05 -0700

    comment btton added

--- src/components/post-view/Comment.js
+++ src/components/post-view/Comment.js
@@ -4,8 +4,15 @@ import { Link } from 'react-router-dom'
 import Default from '../../assets/default.jpg'
 import {userdb} from './../../firebase'
 // import GetProfile from './../../config/getProfile'
+import {FaReply} from 'react-icons/fa'
+import ThumbUpAltOutlinedIcon from '@material-ui/icons/ThumbUpAltOutlined';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import {TextField} from '@material-ui/core'
+import SendIcon from '@material-ui/icons/Send';
+
 export default function Comment(props) {
         const[user, setUser] = useState(null);
+        const[showReply, setShowReply] = useState(false);
        useState(()=>{
          getUser()
        },[]);
@@ -37,12 +44,37 @@ export default function Comment(props) {
                               <text>{user.username}</text>
                             </div>
                             }
-                          
                           <text style={{fontSize:'15px',whiteSpace:'pre-wrap'}}>{props.data.comment}</text>
                         </div>
                     </div>
+                    <div style={{display:'flex',flexDirection:'row',paddingTop:'5px',paddingLeft:'10%'}}>
+                    <div className={'reply-button'}>
+                      <ThumbUpAltIcon style={{marginRight:'3px',fontSize:'16px'}}/>Like
+                      </div>
+                    <div onClick={()=>setShowReply(true)} className={'reply-button'}>
+                      <FaReply style={{marginRight:'3px'}}/>Reply
+                      </div>
+                    <div>
+                      <text style={{fontSize:'14px',marginLeft:'5px',textDecorationLine:'underline',color:'blueviolet',cursor:'pointer'}}>0 Replies</text>
+                    </div>
+                    </div>
+                    <div style={{paddingLeft:'10%',paddingRight:'10%', display:showReply? null: 'none'}}>
+                    <form style={{display:'flex',flexDirection:'row'}}>
+                    <TextField
+                        id="standard-textarea"
+                        placeholder="Reply"
+                        // value={inputComment} onChange={(e)=>setInputComment(e.target.value)}
+                        // disabled = {currentUser ? false : true}
+                        rowsMax={5}
+                        multiline
+                        fullWidth
+                        />
+                    <button type='submit' style={{backgroundColor:'transparent',border:'none'}}>
+                         <SendIcon/>
+                    </button>
+                    </form>
+                    </div>
             <div>
-            
             <hr/>
         </div>
     </div>
