030222ba8f95c480dd8ada45ff5ae02e6956741f
amarpsp10 <amarpsp10@gmail.com>
2021-06-19 02:15:45 -0700

    Feed fetching Optimized

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,43 +1,17 @@
-import React, { useState, useEffect} from 'react'
+import React, { useState, useEffect, useContext} from 'react'
 import './Feed.css'
-import { makeStyles, withStyles } from '@material-ui/core/styles';
 import {useAuth} from './../../contexts/AuthContext'
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
-import NativeSelect from '@material-ui/core/NativeSelect';
-import InputBase from '@material-ui/core/InputBase';
 import FeedItem from './Feed-item'
 import {db} from './../../firebase'
 import Feedback from './../Navigation/FeedBack'
 import CreateTopic from '../Navigation/CreatePost';
 import { Redirect } from 'react-router';
-
+import {useFeedContext} from './../../contexts/FeedContext'
 export default function Feed() {
-    const [loading, setLoading] = useState(true);
-    const [Posts, setPosts] = useState([]);
-    const [age, setAge] = React.useState('');
-    const {currentUser} = useAuth();
-
-    useEffect(() => {
-      GetPosts();
-    }, [])
-
-    async function GetPosts(){
-      try{
-        await db.collection('posts').orderBy('timestamp','desc').onSnapshot(snap=>{
-          setPosts(snap.docs.map(doc=>({id:doc.id,data:doc.data()})))
-        })
-      }catch{
-        alert('something went wrong')
-      }
-       setLoading(false);
-    }
-
-    const handleChange = (event) => {
-      setAge(event.target.value);
-    };
+  const {posts, loading}  = useFeedContext();
+  const {currentUser} = useAuth();
+  
+   
     return (
       <div>
         {!currentUser ? <Redirect to="/welcome" /> : null}
@@ -54,14 +28,14 @@ export default function Feed() {
                       </div>
                       :
                       <div>
-                      {Posts==null ?
+                      {posts==null ?
                            <div className='loading-box'>
                              <div className='loader'></div>
                            </div>
                            :
                            <div>
                            {
-                             Posts.map(data=>{
+                             posts.map(data=>{
                                return <FeedItem id={data.id} data={data.data}/>
                               })
                             }
--- src/components/home-page/Home.js
+++ src/components/home-page/Home.js
@@ -5,16 +5,19 @@ import {Redirect,BrowserRouter as Router, Switch} from 'react-router-dom'
 import Feed from './../Feed/Feed'
 import Footer from './../Footer-pg/Footer'
 import Feedback from './../Navigation/FeedBack'
+import {FeedProvider} from './../../contexts/FeedContext'
 
 export default function Home() {
     const {currentUser} = useAuth()
 
     return (
+        
         <div className={'feed-page'}>
         {currentUser==null ? <Redirect to="/welcome"/>: null}
             <Feed/>
             <Feedback/>
             <Footer/>
         </div>
+        
     )
 }
