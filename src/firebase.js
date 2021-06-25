31be1f98d5ee3d762cd064b82eb6cbffc74686ff
amarpsp10 <amarpsp10@gmail.com>
2021-06-25 08:09:15 -0700

    firebase setup changed

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,13 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCw2v-GaJLGtufhjjIw_auoX5z8N9eNOkY",
-  authDomain: "missioned-forum.firebaseapp.com",
-  databaseURL: "https://missioned-forum-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "missioned-forum",
-  storageBucket: "missioned-forum.appspot.com",
-  messagingSenderId: "796663111568",
-  appId: "1:796663111568:web:04dd0ec1c15d29a16a5d15",
-  measurementId: "G-W9DW5CCGMD"
+  apiKey: "AIzaSyDVSAVDmRiOtoqylORoih-YoC0shIsCUW0",
+  authDomain: "missioned-891c8.firebaseapp.com",
+  databaseURL: "https://missioned-891c8-default-rtdb.firebaseio.com",
+  projectId: "missioned-891c8",
+  storageBucket: "missioned-891c8.appspot.com",
+  messagingSenderId: "55139331955",
+  appId: "1:55139331955:web:a859c24967471be24c1f06"
   };
 
   const app = firebase.initializeApp(firebaseConfig);
